2c34b9c338960556a6868cc6776e2d559742a391
Nhyix <raziel.tole@gmail.com>
2022-01-14 11:56:10 -0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {React,useEffect} from "react"
-import {BrowserRouter,Routes,Route,Link,Navigate} from "react-router-dom";
+import {BrowserRouter,Switch,Route,Link,Redirect} from "react-router-dom";
 import './Firebase'
 import 'bootstrap/dist/css/bootstrap.css';
 import './Sidebar.css'
@@ -102,10 +102,10 @@ function App(props) {
 
         <BrowserRouter>
 
-        <Navigate
+        <Redirect
         from exact="/"
         to="/login">
-        </Navigate>
+        </Redirect>
 
         
         
@@ -161,7 +161,7 @@ function App(props) {
 
            
 
-            <Routes>
+            <Switch>
                 <Route path="/Alta del Cliente" render={() => <Cliente/>}></Route>
                 <Route path="/Baja del Cliente" render={() => <BajaCliente/>} ></Route>
                 <Route path='/Alta del Operador' render={() => <Personal/>}></Route>
@@ -176,7 +176,7 @@ function App(props) {
                 {/* <Route path='/Registro de Inasistencia' render={() => <Asistencia/>}></Route> */}
                
                 
-            </Routes>
+            </Switch>
             
         </BrowserRouter>
 
