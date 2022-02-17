43ed18d2c386670b58f38bacfc2d0f2c6b3959b2
Nhyix <raziel.tole@gmail.com>
2022-02-17 13:41:39 -0600

    fdfds

--- src/Calendario Olga.jsx
+++ src/Calendario Olga.jsx
@@ -19,11 +19,6 @@ import {BrowserRouter,Link,useNavigate} from "react-router-dom";
 
 const CalendarioO = (calendario) => {
 
-  let history = useNavigate();
-
-  const Registro = () => {
-    history("/Calendario O");
-  }
 
 
   const [datos,setDatos] = useState ([])
