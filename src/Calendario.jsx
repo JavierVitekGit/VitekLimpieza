ecb49ff19cfb30de4963d0fec0465e05c80b2132
Nhyix <raziel.tole@gmail.com>
2022-02-18 13:36:45 -0600

    Update Calendario.jsx

--- src/Calendario.jsx
+++ src/Calendario.jsx
@@ -224,6 +224,20 @@ const modClose = () => setModClient(false)
     
 // useEffect(() => {
 
+  const firebaseConfig = {
+    apiKey: "AIzaSyBmZRACI4lPavlz-2N0NyIvTIW9j2DOJhY",
+    authDomain: "androidbrinsk.firebaseapp.com",
+    databaseURL: "https://androidbrinsk-default-rtdb.firebaseio.com",
+    projectId: "androidbrinsk",
+    storageBucket: "androidbrinsk.appspot.com",
+    messagingSenderId: "1038423598895",
+    appId: "1:1038423598895:web:ddfe2d9c575506d192a3da"
+  };
+
+  const app = initializeApp(firebaseConfig)
+
+  const dbRef = getDatabase(app)
+
 
 
   function abcd () {
@@ -246,8 +260,6 @@ const modClose = () => setModClient(false)
   }
 
 
-  const dbRef = getDatabase();
-
   get(child(dbRef,'Operador/')).then((snapshot)=>{
     if(snapshot.exists()){
       snapshot.forEach((childSnapshot)=>{
@@ -269,30 +281,11 @@ function obtener () {
 
  
   
-const firebaseConfig = {
-  apiKey: "AIzaSyBmZRACI4lPavlz-2N0NyIvTIW9j2DOJhY",
-  authDomain: "androidbrinsk.firebaseapp.com",
-  databaseURL: "https://androidbrinsk-default-rtdb.firebaseio.com",
-  projectId: "androidbrinsk",
-  storageBucket: "androidbrinsk.appspot.com",
-  messagingSenderId: "1038423598895",
-  appId: "1:1038423598895:web:ddfe2d9c575506d192a3da"
-};
+
 
     
   
-    
-      const db = getDatabase();
-
-
-
-
-
-
-     
-
-
-      const dbRef = ref(getDatabase());
+  
 
 
       get(child(dbRef,'ClienteUbicacion/' )).then((snapshot) => {
