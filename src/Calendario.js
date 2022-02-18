5d6104e1b2c6818f0e20a458f55f693cb13b2fd2
Nhyix <raziel.tole@gmail.com>
2022-02-18 09:57:45 -0600

    Update Calendario.js

--- src/Calendario.js
+++ src/Calendario.js
@@ -1,4 +1,4 @@
-import {React,useState,useEffect,useCallback} from "react";
+import {React,useState,useCallback} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import './Calendario.css';
 import InfiniteCalendar from 'react-infinite-calendar';
@@ -8,14 +8,17 @@ import {es} from 'date-fns/locale'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-infinite-calendar/styles.css';
 import { initializeApp } from 'firebase/app';
-import {getDatabase,ref,child,get,update, set} from "firebase/database";
+import {getDatabase,ref,child,get,update} from "firebase/database";
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
-import SideBar from './Sidebar'
+import SideBarO from "./SideBarO";
+import Login from "./login";
+import {BrowserRouter,Link,useNavigate} from "react-router-dom";
 
 
 
-const Calendario = (calendario) => {
+const CalendarioO = (calendario) => {
+
 
 
   const [datos,setDatos] = useState ([])
@@ -28,6 +31,75 @@ const Calendario = (calendario) => {
   const [select,setSelect] = useState([])
 
   const [client,setClient] = useState('')
+
+  const [arrayPersonal,setArrayPersonal] = useState([]);
+
+
+  //Datos Olga
+
+  const [arrayClientCl,setArrayClientCl] = useState([]);
+
+  const [arrayJusti,setArrayJusti] = useState([]);
+
+ 
+
+      arrayClientCl.sort()
+
+
+  const [namae,setNamae] = useState([]);
+
+  const unicosTable = [];
+
+  const unicos = [];
+
+  unicos.push("")
+
+  namae.forEach((item)=>{
+    if (!unicos.includes(item)){
+      unicos.push(item)
+    }
+  });
+
+ 
+
+
+// C L I E N T E
+
+  const unicoss = [];
+
+  unicoss.push("");
+
+  arrayClientCl.forEach((item)=> {
+    if (!unicoss.includes(item) && !unicosTable.includes(item)){
+      unicoss.push(item)
+      unicosTable.push(item)
+    }
+  })
+
+  unicoss.sort()
+
+  const [selClient,setSelCliente] = useState("")
+
+  console.log("Select",selClient)
+
+
+
+// P E R S O N A L
+
+const [arrayp,setArrayP] = useState([]);
+
+arrayJusti.forEach((item)=> {
+  if (!arrayp.includes(item)){
+    arrayp.push(item)
+
+    arrayp.sort()
+
+  }
+})
+
+
+
+
 
 
 // F I L T E R 
@@ -44,6 +116,26 @@ const Calendario = (calendario) => {
   client.clienteC.toLowerCase().includes(search.toLowerCase()) || client.name.toLowerCase().includes(search.toLowerCase())
   );
 
+// M O D A L
+
+const[modal,setModal] =useState(false)
+const handleShow = () => setModal(true)
+const handleClose = () => setModal(false)
+
+
+const [modd,setModd] = useState(false)
+const handleShoww = () => setModd(true)
+const handleClosee = () => setModd(false)
+
+
+const [mod,setMod] = useState(false)
+const show = () => setMod(true)
+const close = () => setMod(false)
+
+
+const [modClient,setModClient] = useState(false)
+const modOpen = () => setModClient(true)
+const modClose = () => setModClient(false)
 
 
 // C A L E N D A R I O
@@ -56,22 +148,20 @@ const Calendario = (calendario) => {
 
   const [infinite,setInfinite] = useState(new Date())
 
-  // var infiniteF = infinite.getDate
 
-  // console.log(infiniteF)
 
   var dia = infinite.toISOString().substring(8,10)
 
   var mes = infinite.toISOString().substring(5,7)
 
-  var año = infinite.toISOString().substring(0,4)
+  var anio = infinite.toISOString().substring(0,4)
   
   
     var today = new Date();
     var hora = today.getHours() 
 
 
-    var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 2);
+    var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 3);
     formatRelative(subDays(new Date(), 3), new Date(), { locale: es })
 
 
@@ -95,9 +185,9 @@ const Calendario = (calendario) => {
 
 
 
-
     function mostrarRegistro () {
       setMostrar(false)
+
     }
 
     function mostrarCalendario () {
@@ -134,6 +224,51 @@ const Calendario = (calendario) => {
 
 // F I R E B A S E
     
+// useEffect(() => {
+
+
+
+  function abcd () {
+
+    if (selClient == "") {
+      modOpen()
+    } else {
+      obtener();
+    }
+
+    
+
+  }
+
+  function regresar () {
+    setSelCliente("")
+    mostrarCalendario();
+    setArrayJusti([])
+    setNamae([])
+  }
+
+
+  const dbRef = ref(getDatabase());
+
+  get(child(dbRef,'Operador/')).then((snapshot)=>{
+    if(snapshot.exists()){
+      snapshot.forEach((childSnapshot)=>{
+        var sup = childSnapshot.child("Supervisor").val()
+        var cliente = childSnapshot.child("Cliente").val()
+
+    
+          arrayClientCl.push(cliente)
+          
+     
+
+      })
+    }
+  })
+
+
+function obtener () {
+
+
  
   
 const firebaseConfig = {
@@ -147,33 +282,42 @@ const firebaseConfig = {
 };
 
     
-      const app = initializeApp(firebaseConfig);
-      console.log(app)
+  
     
       const db = getDatabase();
 
-      function abcd () {
-        obtener();
-
-      }
-
-      function regresar () {
-        setArray([]);
-        mostrarCalendario();
-      }
 
 
-      function obtener () {
+
+
 
      
 
 
       const dbRef = ref(getDatabase());
 
-      get(child(dbRef,'Asistencia/' + dia + "-" + mes + "-" + año)).then((snapshot)=> {
+
+      get(child(dbRef,'ClienteUbicacion/' )).then((snapshot) => {
+        if(snapshot.exists()) {
+
+          snapshot.forEach((childSnapshot)=> {
+            var name = childSnapshot.child("Nombre").val()
+            var personal = childSnapshot.child("Personal").val()
+
+            if(name==selClient){
+              arrayPersonal.push(personal)
+            }
+
+          })
+
+     
+
+         console.log("Razi:",arrayPersonal)
+        }
+      })
 
 
-
+      get(child(dbRef,'Asistencia/' + dia + "-" + mes + "-" + anio)).then((snapshot)=> {
           if (snapshot.exists()){
             setDatos([])
               snapshot.forEach((childSnapshot)=>{
@@ -189,137 +333,96 @@ const firebaseConfig = {
               })
           }
       })
-
+      
 
       get(child(dbRef,'Operador/')).then((snapshot)=>{
           if (snapshot.exists()){
               setComp([])
               snapshot.forEach((childSnapshot)=>{
+
+                  var sup = childSnapshot.child("Supervisor").val()
                   var clienteOp = childSnapshot.child("Cliente").val()
                   var nombreOp = childSnapshot.child("Nombre").val()
                   var hora = childSnapshot.child("Horario").val()
                   comp.push({clienteC:clienteOp,name:nombreOp,hr:hora})
-                  
-                  
+
+                
+
               })
 
-              console.log("Operador:",datos)
+            comp.forEach((iter)=> {
+              if (iter.clienteC == selClient) {
+                arrayJusti.push({clienteC:iter.clienteC,name:iter.name,hr:iter.hr})
+                arrayJusti.sort();
 
-            //   var aux = [];
+                console.log("Justi",arrayJusti.length)
 
-            //   console.log("Validación:",aux)
-
-              
-              console.log("New Array",array)
-
-              // setArray([])
-
-             
-
-              for (var i = 0; i < comp.length; i++) {
-                  var igual=false;
-                   for (var j = 0; j < datos.length & !igual; j++) {
-                       if(comp[i]['name'] == datos[j]['nombre']) 
-                               igual=true;
-                   }
-                  if(!igual)array.push(comp[i]);
-
-                  
-
-                var alpha =  array.sort((a,b) => {
-                    if (a.clienteC < b.clienteC) return -1;
-                    if (a.clienteC > b.clienteC) return 1
-
-                    return 0;
-                  })
-
-                  
-                  
-              
-                  setTimeout(() => {
-                    mostrarRegistro();
-                  }, 100);
               }
+            })
+
+            comp.forEach((iter)=>{
+              if (iter.clienteC == selClient) {
+                namae.push(iter.name)
+                namae.sort()
+              }
+            } )
             
-
-      
-          
-
+            
 
           }
       })
 
-      comp.forEach(iter => {
-        if (iter.clienteC == client) {
-          select.push(iter.name)
+      setTimeout(() => {
+
+        console.log("Personal::: "+arrayPersonal[0])
+        for(var i=0; i<arrayPersonal[0]; i++){
+          if (arrayJusti[i] == undefined){
+            arrayJusti.push({clienteC:selClient,name:"Vacante",hr:"08:00"})
+          }else{
+
+          }
+
         }
-      })
 
+        mostrarRegistro();
+      }, 500);
 
-      // filtered.forEach(iter => {
-      //   comp.forEach(other => {
-      //     if (iter.clienteC == other.clienteC) {
-      //         select.push(other.name)
-      //     }
-      //   })
-      // })
-
-
+      setArrayPersonal([])
+      
 
       console.log("Select:", select)
 
-}
 
+      
 
-
-
-
-
-// H I D D E N
-
-function showContent() {
-var element = document.getElementById("hidden")
-var check = document.getElementById("suplenciaCheck")
-
-  if(check.checked) {
-    element.style.display='block';
-  }
-    else {
-      element.style.display='none';
-    }
-
-};
 
 //   M O D A L  
 
 
 
 
-
-  const[modal,setModal] =useState(false)
-  const handleShow = () => setModal(true)
-  const handleClose = () => setModal(false)
+ 
+}
 
 
-  const [modd,setModd] = useState(false)
-  const handleShoww = () => setModd(true)
-  const handleClosee = () => setModd(false)
+// console.log("ArrayJusti:",arrayJusti)
+// arrayJusti.forEach((item)=>{
+//   if(item.suplencia ==""){
+//     item.suplencia == "No se cubrio"
+//   }
+// })
 
 
-  const [mod,setMod] = useState(false)
-  const show = () => setMod(true)
-  const close = () => setMod(false)
+function writeJustiData(event) {
+  event.preventDefault()
+console.log('Justificaciones/' + dia + "-" + mes + "-" + anio + "/" + selClient)
+  update(ref(getDatabase(),'Justificaciones/' + dia + "-" + mes + "-" + anio + "/" + selClient),{
+    Datos:arrayJusti
+  });
 
+}
 
-
-
-    
-//     R  E  N  D  E  R  
-  
-
-
-
-
+// },[])
 
 
 
@@ -346,30 +449,32 @@ mostrar?
 
 
 <div className="Calendario" id="inf">
-
-
-  <div className="adminSide">
-    <SideBar></SideBar>
-  </div>
+  
+{/* <div className="SideOlgaBb">
+            <SideBarO/>
+            
+            </div> */}
 
   <div className="calH">
 
 <h1 id="he">
 <i id="calendarI" class="bi bi-calendar-week-fill"></i>
-  Calendario
+  Lista de Clientes
 </h1>
 
 </div>
-<div className="divCale"></div>
+
 
   <div className="containerCal">
 
+    <br/>
+
 <InfiniteCalendar className="Cal"  
-width={500}
+width={600}
 height={250}
 selected={false}
 minDate={lastWeek}
-maxDate={nextWeek}
+maxDate={today}
 onSelect={setInfinite}
 locale={{
   locale:require('date-fns/locale/es'),
@@ -387,11 +492,90 @@ displayOptions={{
 
 
 
-<p id="txtcal"></p>
-<p id="infiniteV"></p>
 
 <br></br>
 
+<select onClick={forceUpdate} id="selClient" onChange={v=>{setSelCliente(v.target.value)}}>
+  {unicoss.map((item)=> <option>{item}</option>)}
+</select>
+
+<br/>
+
+
+{/* <table id="estateTable" class="table table-striped" >
+<thead class="table-dark">
+<tr >
+  <th scope="col">Cliente/Ubicación</th>
+  <th scope="col">Estado</th>
+
+
+</tr>
+</thead>
+
+<tbody>
+{ unicosTable.map((item)=> 
+
+
+
+
+{
+  
+  // if (hora >= item.hr.substr(0,2)  ) {
+    
+    
+
+    return (
+
+    <tr>
+
+    <td id="testSelect" onChange={v=>{setClient(v.target.value)}}>
+      
+       {item}
+      
+    </td>
+    <td>
+
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-clipboard2-check" viewBox="0 0 16 16" color="green">
+  <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5h3Z"/>
+  <path d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-12Z"/>
+  <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3Z"/>
+</svg>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-clipboard2-minus" viewBox="0 0 16 16" color="orange">
+  <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5h3Z"/>
+  <path d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-12Z"/>
+  <path d="M6 8a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1H6Z"/>
+</svg>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-clipboard2-x" viewBox="0 0 16 16" color="red">
+  <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5h3Z"/>
+  <path d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-12Z"/>
+  <path d="M8 8.293 6.854 7.146a.5.5 0 1 0-.708.708L7.293 9l-1.147 1.146a.5.5 0 0 0 .708.708L8 9.707l1.146 1.147a.5.5 0 0 0 .708-.708L8.707 9l1.147-1.146a.5.5 0 0 0-.708-.708L8 8.293Z"/>
+</svg>
+    </td>
+    <td></td> 
+
+  
+   
+   
+
+   
+    </tr>
+    )
+
+
+// } 
+// else {
+
+
+// }
+
+}
+ )}
+ 
+</tbody>
+</table> */}
 
 
 <input class="btn btn-success" type="submit" value="Ir al Registro" onClick={abcd} id="calbt"></input>
@@ -445,6 +629,56 @@ Ok
 </Modal>
 
 
+
+<Modal className="modal-container" 
+      show={modClient}  
+      onHide={modClose } 
+      animation={true} 
+      backdrop="static" 
+      keyboard={false}   
+      {...calendario}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered>
+
+
+<Modal.Header>
+
+
+<Modal.Title>No se ha seleccionado ningun cliente</Modal.Title>
+
+
+</Modal.Header>
+
+
+<Modal.Body>
+
+
+<p>Seleccione un cliente para poder continuar</p>
+
+
+</Modal.Body>
+
+
+<Modal.Footer>
+
+
+  <Button variant="danger" onClick={modClose}>
+
+
+Ok
+
+
+  </Button>
+
+
+</Modal.Footer>
+
+
+</Modal>
+
+
+
 </div>
 
 
@@ -474,17 +708,31 @@ Ok
 
 
 <div className="Background" id="Registro"  >
+{/* <div className="SideOlgas">
+            <SideBarO/>
+            
+            </div> */}
+
 
   <div className="regIH">
 
+
 <h1 id="head">
+  
   <i id="calendarX" class="bi bi-calendar-x"></i>
   Justificaciones
-  <h1 className="dateCa">{dia + "-" + mes + "-" + año}</h1>
+  <h1 className="dateCa">{dia + "-" + mes + "-" + anio}</h1>
   </h1>
+
+  
+
+
+
+
 </div>
 
 <table class="table table-striped" id="Tabla">
+  
 <thead class="table-dark">
 <tr id="headertab">
   <th scope="col">Cliente/Ubicación</th>
@@ -498,14 +746,14 @@ Ok
 
 
 
-{ filtered.map((item)=> 
+{ arrayJusti.map((item)=> 
 
 
 
 
 {
   
-  if (hora >= item.hr.substr(0,2)  ) {
+  // if (hora >= item.hr.substr(0,2)  ) {
     
     
 
@@ -521,27 +769,35 @@ Ok
     <td>{item.name}</td> 
     <td>{item.hr}
     <br/>
-    <select className="estados" onChange={v=> item.asis = v.target.value}  >
-      <option>Justificada</option>
-      <option>Injustificada</option>
+    <select className="estados" onChange={v=>item.estado = v.target.value }  >
+      <option></option>
+      <option>Descanso</option>
       <option>Incapacidad</option>
+      <option>Injustificada</option>
+      <option>Justificada</option>
       <option>Vacaciones</option>
 
 
       
       </select>
       <br/>
-      <textarea className="txtArea" placeholder="Motivo de la asistencia"  onChange={v=>item.justi = v.target.value}></textarea> 
+      <br/>
+      <textarea className="txtArea" placeholder="Motivo de la Justificacion"  onChange={v=>item.justi = v.target.value}></textarea> 
     </td> 
    
     <td>
-      <input id="suplenciaCheck" type="checkbox"/>
       <div id="hidden">
     
-        <select onClick={forceUpdate} value={select} onChange={v=>{setSelect(v.target.value)}} >
+        <br/>
+                                                                                       {/* v=>{setSeleitect(v.target.value) */}
+        <select className="selectName" onClick={forceUpdate}  onChange={v=>item.asis = v.target.value} >
+          {unicos.map((item) => <option>{item}</option>)}
 
         </select>
 
+        <p>Otro</p>
+
+        <textarea placeholder="Nombre del suplente" onChange={v=>item.suplencia = v.target.value}></textarea>
       </div>
     </td>
 
@@ -554,11 +810,11 @@ Ok
     )
 
 
-} 
-else {
+// } 
+// else {
 
 
-}
+// }
 
 }
  )}
@@ -645,7 +901,7 @@ Ok
 
 
 
-<p>El registro de la asistencia ha sido realizado con exito</p>
+<p>El registro de justificaciones ha sido realizado con exito</p>
 
 
 </Modal.Body>
@@ -670,17 +926,22 @@ Ok
 
 
 
-  
+
 
 <p id="dia"></p>
+
+<br/>
 
 <div class = "btn-groupp">
 
 
 
-<input class="btn btn-success" type="submit" value="Guardar" onClick={show}  id="btt"></input>
+<input class="btn btn-success" type="submit" value="Guardar" onClick={writeJustiData}  id="btt"></input>
 
 <input class="btn btn-secondary"  type="submit" value="Regresar al Calendario" onClick={regresar} id="bt2"></input>
+
+<br/>
+<br/>
 
 
 </div>    
@@ -705,4 +966,4 @@ Ok
 }
 
 
-export default Calendario
+export default CalendarioO;
