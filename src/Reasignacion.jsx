75fbb487ebf2a355deb6b65912036045e140061f
Nhyix <raziel.tole@gmail.com>
2022-02-19 10:38:06 -0600

    Update Reasignacion.jsx

--- src/Reasignacion.jsx
+++ src/Reasignacion.jsx
@@ -238,7 +238,7 @@ return(
 
         <br />
 
-        <select onClick={forceUpdate} value={tel} onChange={v=> setTel(v.target.value),handlerNombres}>
+        <select onClick={forceUpdate} value={tel} onChange={v=> setTel(v.target.value)}>
         {nombrecitos.map((item)=> <option value={item.nm}>{item}</option>)}    
         </select>
           <br/>
