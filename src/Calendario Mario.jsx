ab7a83b7174167973265f828e8855ecbe9d2f9b8
Nhyix <raziel.tole@gmail.com>
2022-02-21 15:40:01 -0600

    Update Calendario Mario.jsx

--- src/Calendario Mario.jsx
+++ src/Calendario Mario.jsx
@@ -18,6 +18,17 @@ import Login from "./login";
 
 const CalendarioM = (calendario) => {
 
+  const firebaseConfig = {
+    apiKey: "AIzaSyBmZRACI4lPavlz-2N0NyIvTIW9j2DOJhY",
+    authDomain: "androidbrinsk.firebaseapp.com",
+    databaseURL: "https://androidbrinsk-default-rtdb.firebaseio.com",
+    projectId: "androidbrinsk",
+    storageBucket: "androidbrinsk.appspot.com",
+    messagingSenderId: "1038423598895",
+    appId: "1:1038423598895:web:ddfe2d9c575506d192a3da"
+  };
+
+
   const app = initializeApp(firebaseConfig);
   console.log(app)
 
@@ -272,15 +283,7 @@ function obtener () {
 
  
   
-const firebaseConfig = {
-  apiKey: "AIzaSyBmZRACI4lPavlz-2N0NyIvTIW9j2DOJhY",
-  authDomain: "androidbrinsk.firebaseapp.com",
-  databaseURL: "https://androidbrinsk-default-rtdb.firebaseio.com",
-  projectId: "androidbrinsk",
-  storageBucket: "androidbrinsk.appspot.com",
-  messagingSenderId: "1038423598895",
-  appId: "1:1038423598895:web:ddfe2d9c575506d192a3da"
-};
+
 
     
       
