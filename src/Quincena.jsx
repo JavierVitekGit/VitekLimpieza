67b0af105bb105f266eb42b705134f93121c9099
Nhyix <raziel.tole@gmail.com>
2022-02-22 12:43:27 -0600

    Update Quincena.jsx

--- src/Quincena.jsx
+++ src/Quincena.jsx
@@ -118,7 +118,7 @@ const Quincena = (quincena) => {
 
             <tbody>
 
-                {justificaciones.map((item)=>{
+                {justificaciones.forEach((item)=>{
 
                     return (
 
