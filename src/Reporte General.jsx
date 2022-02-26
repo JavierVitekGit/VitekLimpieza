807d06f6166380e35b5968565ab75fe204b9a319
Nhyix <raziel.tole@gmail.com>
2022-02-26 14:35:35 -0600

    XdXdX XD XED

--- src/Reporte General.jsx
+++ src/Reporte General.jsx
@@ -203,10 +203,10 @@ const ReporteG = (reporte) => {
 
                                                    item.dias.map((dialokobydiego)=>{
 
-                                                    console.log(""+iter.Fecha.substring(0,2) +"=="+ dialokobydiego.key);
+                                                    console.log(""+iter.Fecha.substring(0,2) +"=="+ Object.keys(dialokobydiego)[0]  );
                                                     console.log(dialokobydiego)
                                                     console.log(item.Nombre+"=="+iter.Nombre);
-                                                        if (item.Nombre == iter.Nombre && iter.Fecha.substring(0,2) == dialokobydiego.key ) {
+                                                        if (item.Nombre == iter.Nombre && +iter.Fecha.substring(0,2) == +Object.keys(dialokobydiego)[0] ) {
                                                             dialokobydiego = {[dialokobydiego.key]:iter.Estado}
                                                         }
                                                        
