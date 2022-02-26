af4e7c1289af69c610ca4be26391672ae2b986c7
Nhyix <raziel.tole@gmail.com>
2022-02-26 17:07:31 -0600

    Update Reporte General.jsx

--- src/Reporte General.jsx
+++ src/Reporte General.jsx
@@ -209,7 +209,7 @@ const ReporteG = (reporte) => {
                                                     // console.log(dialokobydiego)
                                                     // console.log(item.Nombre+"=="+iter.Nombre);
                                                         if (item.Nombre == iter.Nombre && +iter.Fecha.substring(0,2) == +Object.keys(dialokobydiego)[0] ) {
-                                                            console.log("Aqui se encontro algo :",{[Object.keys(dialokobydiego)[0]]:iter.Estado},"En el index:: ", index)
+                                                          
                                                             item.dias[index] = {[Object.keys(dialokobydiego)[0]]:iter.Estado+" "+iter.Justificacion+" "+iter.Suplencia}
                                                         }
                                                         index++;
