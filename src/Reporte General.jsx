75cf527ac16706020e76851522a7982da6a46ead
Nhyix <raziel.tole@gmail.com>
2022-02-28 15:30:50 -0600

    Update Reporte General.jsx

--- src/Reporte General.jsx
+++ src/Reporte General.jsx
@@ -369,66 +369,38 @@ const ReporteG = (reporte) => {
 
 
 
-        var Utf8 = {
- 
-            // public method for url encoding
-            encode : function (string) {
-                string = string.replace(/\r\n/g,"\n");
-                var utftext = "";
-         
-                for (var n = 0; n < string.length; n++) {
-         
-                    var c = string.charCodeAt(n);
-         
-                    if (c < 128) {
-                        utftext += String.fromCharCode(c);
-                    }
-                    else if((c > 127) && (c < 2048)) {
-                        utftext += String.fromCharCode((c >> 6) | 192);
-                        utftext += String.fromCharCode((c & 63) | 128);
-                    }
-                    else {
-                        utftext += String.fromCharCode((c >> 12) | 224);
-                        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                        utftext += String.fromCharCode((c & 63) | 128);
-                    }
-         
-                }
-         
-                return utftext;
-            },
-         
-            // public method for url decoding
-            decode : function (utftext) {
-                var string = "";
-                var i = 0;
-                var c = c1 = c2 = 0;
-         
-                while ( i < utftext.length ) {
-         
-                    c = utftext.charCodeAt(i);
-         
-                    if (c < 128) {
-                        string += String.fromCharCode(c);
-                        i++;
-                    }
-                    else if((c > 191) && (c < 224)) {
-                        c2 = utftext.charCodeAt(i+1);
-                        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                        i += 2;
-                    }
-                    else {
-                        c2 = utftext.charCodeAt(i+1);
-                        c3 = utftext.charCodeAt(i+2);
-                        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                        i += 3;
-                    }
-         
-                }
-         
-                return string;
+        function Utf8ArrayToStr(array) {
+            var out, i, len, c;
+            var char2, char3;
+        
+            out = "";
+            len = array.length;
+            i = 0;
+            while(i < len) {
+            c = array[i++];
+            switch(c >> 4)
+            { 
+              case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+                // 0xxxxxxx
+                out += String.fromCharCode(c);
+                break;
+              case 12: case 13:
+                // 110x xxxx   10xx xxxx
+                char2 = array[i++];
+                out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+                break;
+              case 14:
+                // 1110 xxxx  10xx xxxx  10xx xxxx
+                char2 = array[i++];
+                char3 = array[i++];
+                out += String.fromCharCode(((c & 0x0F) << 12) |
+                               ((char2 & 0x3F) << 6) |
+                               ((char3 & 0x3F) << 0));
+                break;
             }
-         
+            }
+        
+            return out;
         }
 
 
@@ -440,7 +412,7 @@ const ReporteG = (reporte) => {
     var dataType = 'application/vnd.ms-excel';
     var tableSelect = document.getElementById('generate');
     // var tableHTML = encodeUtf8(tableSelect.outerHTML)
-    var tableHTML = Utf8(tableSelect.outerHTML)
+    var tableHTML = Utf8ArrayToStr(tableSelect.outerHTML)
     // var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
     
 
