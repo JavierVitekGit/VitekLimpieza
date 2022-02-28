82e44ad5209fa389084f7dbf6f37a58e4b002f03
Nhyix <raziel.tole@gmail.com>
2022-02-28 15:54:18 -0600

    Update Reporte General.jsx

--- src/Reporte General.jsx
+++ src/Reporte General.jsx
@@ -369,10 +369,10 @@ const ReporteG = (reporte) => {
 
 
 
-        
+        var Utf8 = {
  
             // public method for url encoding
-             function encode (string) {
+            encode : function (string) {
                 string = string.replace(/\r\n/g,"\n");
                 var utftext = "";
          
@@ -396,11 +396,40 @@ const ReporteG = (reporte) => {
                 }
          
                 return utftext;
-            }
+            },
          
             // public method for url decoding
-           
-        
+            decode : function (utftext) {
+                var string = "";
+                var i = 0;
+                var c = c1 = c2 = 0;
+         
+                while ( i < utftext.length ) {
+         
+                    c = utftext.charCodeAt(i);
+         
+                    if (c < 128) {
+                        string += String.fromCharCode(c);
+                        i++;
+                    }
+                    else if((c > 191) && (c < 224)) {
+                        c2 = utftext.charCodeAt(i+1);
+                        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                        i += 2;
+                    }
+                    else {
+                        c2 = utftext.charCodeAt(i+1);
+                        c3 = utftext.charCodeAt(i+2);
+                        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                        i += 3;
+                    }
+         
+                }
+         
+                return string;
+            }
+         
+        }
 
 
 
@@ -411,8 +440,8 @@ const ReporteG = (reporte) => {
     var dataType = 'application/vnd.ms-excel';
     var tableSelect = document.getElementById('generate');
     // var tableHTML = encodeUtf8(tableSelect.outerHTML)
-    // var tableHTML = encode(tableSelect.outerHTML)
-    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+    var tableHTML = Utf8.encode(tableSelect.outerHTML)
+    // var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
     
 
     
