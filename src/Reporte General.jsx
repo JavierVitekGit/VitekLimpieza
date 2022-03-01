f4547ee7ce476b42b4e11d44701fb4df72f0d5d7
Nhyix <raziel.tole@gmail.com>
2022-03-01 09:40:14 -0600

    Update Reporte General.jsx

--- src/Reporte General.jsx
+++ src/Reporte General.jsx
@@ -363,20 +363,20 @@ const ReporteG = (reporte) => {
     // var tableHTML = tableSelect.innerHTML.toString()
     // var tableHTML = btoa(tableSelect.outerHTML)
 
-    var tableHTML = (tableSelect.outerHTML.replace(/ñ/g, '&ntilde;'),
-                     tableSelect.outerHTML.replace(/Ñ/g, '&Ntilde;'),
-                     tableSelect.outerHTML.replace(/á/g, '&aacute;'),
-                     tableSelect.outerHTML.replace(/Á/g, '&Aacute;'),
-                     tableSelect.outerHTML.replace(/é/g, '&eacute;'),
-                     tableSelect.outerHTML.replace(/É/g, '&Eacute;'),
-                     tableSelect.outerHTML.replace(/í/g, '&iacute;'),
-                     tableSelect.outerHTML.replace(/Í/g, '&Iacute;'),
-                     tableSelect.outerHTML.replace(/ó/g, '&oacute;'),
-                     tableSelect.outerHTML.replace(/Ó/g, '&Oacute;'),
-                     tableSelect.outerHTML.replace(/ú/g, '&uacute;'),
-                     tableSelect.outerHTML.replace(/Ú/g, '&Uacute;'),
-                     tableSelect.outerHTML.replace(/º/g, '&ordm;')
-                     )
+    var tableHTML =  tableSelect.outerHTML.replace(/ñ/g, '&ntilde;');
+                     tableSelect.outerHTML.replace(/Ñ/g, '&Ntilde;');
+                     tableSelect.outerHTML.replace(/á/g, '&aacute;');
+                     tableSelect.outerHTML.replace(/Á/g, '&Aacute;');
+                     tableSelect.outerHTML.replace(/é/g, '&eacute;');
+                     tableSelect.outerHTML.replace(/É/g, '&Eacute;');
+                     tableSelect.outerHTML.replace(/í/g, '&iacute;');
+                     tableSelect.outerHTML.replace(/Í/g, '&Iacute;');
+                     tableSelect.outerHTML.replace(/ó/g, '&oacute;');
+                     tableSelect.outerHTML.replace(/Ó/g, '&Oacute;');
+                     tableSelect.outerHTML.replace(/ú/g, '&uacute;');
+                     tableSelect.outerHTML.replace(/Ú/g, '&Uacute;');
+                     tableSelect.outerHTML.replace(/º/g, '&ordm;');
+                     
 
     // tableHTML.replace(/'á'/g, 'á');
     // tableHTML.replace(/'é'/g, 'é');
