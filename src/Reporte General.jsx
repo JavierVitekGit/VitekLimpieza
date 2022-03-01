1d4259e469daf4bd2f1eacaf7ca176f554e296ef
Nhyix <raziel.tole@gmail.com>
2022-03-01 09:14:07 -0600

    Update Reporte General.jsx

--- src/Reporte General.jsx
+++ src/Reporte General.jsx
@@ -320,116 +320,36 @@ const ReporteG = (reporte) => {
     })
 
 
+    // F U N C I O N   P E R R O N A
     
     
-    //    var datosReporte=[];
-
-    //     datos.map((fila)=>{
-    //         var dataFila = [];
-            
-    //         dataFila.push({"Nombre":fila.Nombre});
-              
-    //         var cnt = 0
-    //         fila.dias.map((v)=>{
-    //            dataFila.push({[arrayD[cnt]]:v[+arrayD[cnt]]})
-    //            cnt ++;
-    //         })
-            
-    //        datosReporte.push(dataFila);     
-    //     })
-
-    //     var final = Object.values(datosReporte)
-
-
-  
-    //     var datosTest = Object.values(datos)
+    // function funcionPerrona(){
+    //     var datosReporte=[];
+ 
+    //      datos.map((fila)=>{
+    //          var dataFila = [];
+ 
+    //          dataFila.push({"Nombre":fila.Nombre});
+ 
+    //          var cnt = 0
+    //          fila.dias.map((v)=>{
+    //             dataFila.push({[arrayD[cnt]]:v[""+arrayD[cnt]]})
+    //             cnt ++;
+    //          })
+ 
+    //         datosReporte.push(dataFila);     
+    //      })
+ 
+    //      return datosReporte;
+ 
+ 
+    //  }
+ 
 
     // E N C O D E
 
 
-        // function encodeUtf8(text) {
-        //     const code = encodeURIComponent(text);
-        //     const bytes = [];
-        //     for (var i = 0; i < code.length; i++) {
-        //         const c = code.charAt(i);
-        //         if (c === '%') {
-        //             const hex = code.charAt(i + 1) + code.charAt(i + 2);
-        //             const hexVal = parseInt(hex, 16);
-        //             bytes.push(hexVal);
-        //             i += 2;
-        //         } else bytes.push(c.charCodeAt(0));
-        //     }
-        //     return bytes;
-        // }
-
-
-
-        // var Utf8 = {
- 
-        //     // public method for url encoding
-        //     encode : function (string) {
-        //         string = string.replace(/\r\n/g,"\n");
-        //         var utftext = "";
-         
-        //         for (var n = 0; n < string.length; n++) {
-         
-        //             var c = string.charCodeAt(n);
-         
-        //             if (c < 128) {
-        //                 utftext += String.fromCharCode(c);
-        //             }
-        //             else if((c > 127) && (c < 2048)) {
-        //                 utftext += String.fromCharCode((c >> 6) | 192);
-        //                 utftext += String.fromCharCode((c & 63) | 128);
-        //             }
-        //             else {
-        //                 utftext += String.fromCharCode((c >> 12) | 224);
-        //                 utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-        //                 utftext += String.fromCharCode((c & 63) | 128);
-        //             }
-         
-        //         }
-         
-        //         return utftext;
-        //     },
-         
-        //     // public method for url decoding
-        //     decode : function (utftext) {
-        //         var string = "";
-        //         var i = 0;
-        //         var c = c1 = c2 = 0;
-         
-        //         while ( i < utftext.length ) {
-         
-        //             c = utftext.charCodeAt(i);
-         
-        //             if (c < 128) {
-        //                 string += String.fromCharCode(c);
-        //                 i++;
-        //             }
-        //             else if((c > 191) && (c < 224)) {
-        //                 c2 = utftext.charCodeAt(i+1);
-        //                 string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-        //                 i += 2;
-        //             }
-        //             else {
-        //                 c2 = utftext.charCodeAt(i+1);
-        //                 c3 = utftext.charCodeAt(i+2);
-        //                 string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-        //                 i += 3;
-        //             }
-         
-        //         }
-         
-        //         return string;
-        //     }
-         
-        // }
-
-
-
-
-
+      
 
 
 
@@ -443,7 +363,7 @@ const ReporteG = (reporte) => {
     // var tableHTML = tableSelect.innerHTML.toString()
     // var tableHTML = btoa(tableSelect.outerHTML)
 
-    var tableHTML = tableSelect.outerHTML;
+    var tableHTML = tableSelect.outerHTML.replace(/ñ/g,);
 
     tableHTML.replace(/'á'/g, 'á');
     tableHTML.replace(/'é'/g, 'é');
@@ -457,7 +377,7 @@ const ReporteG = (reporte) => {
     tableHTML.replace(/'Í'/g, '&Iacute;');
     tableHTML.replace(/'Ã³'/g, '&Oacute;');
     tableHTML.replace(/'Ú'/g, '&Uacute;');
-    tableHTML.replace(/'ñ'/g, 'ñ');
+    tableHTML.replace(/'ñ'/g, '&ntilde;');
     tableHTML.replace(/'Ñ‘'/g, 'Ñ');
     // var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
     
