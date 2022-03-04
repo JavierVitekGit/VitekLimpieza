873a3d1454b641a4e79c61e4eb08f17031d445bb
Nhyix <raziel.tole@gmail.com>
2022-03-04 15:19:22 -0600

    Update Calendario.jsx

--- src/Calendario.jsx
+++ src/Calendario.jsx
@@ -272,13 +272,13 @@ const modClose = () => setModClient(false)
   })
 
 
-  function toggle(elemento){
-    if(elemento.value=="Si"){
-      document.getElementById("supvisible").style.display = "block";
-    } else {
-      document.getElementById("supvisible").style.display = "none";
-    }
-  }
+  // function toggle(elemento){
+  //   if(elemento.value=="Si"){
+  //     document.getElementById("supvisible").style.display = "block";
+  //   } else {
+  //     document.getElementById("supvisible").style.display = "none";
+  //   }
+  // }
 
 
 function obtener () {
