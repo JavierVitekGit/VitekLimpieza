52643c4ab491de74d564de35266ac73f91d1fa04
Nhyix <raziel.tole@gmail.com>
2022-03-04 15:43:50 -0600

    Update Calendario.jsx

--- src/Calendario.jsx
+++ src/Calendario.jsx
@@ -272,13 +272,13 @@ const modClose = () => setModClient(false)
   })
 
 
-  // function toggle(elemento){
-  //   if(elemento.value=="Si"){
-  //     document.getElementById("supvisible").style.display = "block";
-  //   } else {
-  //     document.getElementById("supvisible").style.display = "none";
-  //   }
-  // }
+  function toggle(elemento){
+    if(elemento.value=="Si"){
+      document.getElementById("supvisible").style.display = "block";
+    } else {
+      document.getElementById("supvisible").style.display = "none";
+    }
+  }
 
 
 function obtener () {
@@ -851,7 +851,7 @@ Ok
 
         <br/>
 
-        <input type="radio" name="supradio"  value="No"></input>
+        <input type="radio" name="supradio" onClick={toggle(this)}  value="No"></input>
         <label class="form-outline-label">No</label>
 
 {/*                                                                              
