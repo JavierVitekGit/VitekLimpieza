1b663849659dabcd18a5679c114464a7a42b73ad
Nhyix <raziel.tole@gmail.com>
2022-03-07 12:33:51 -0600

    Update QuincenaPrueba.jsx

--- src/QuincenaPrueba.jsx
+++ src/QuincenaPrueba.jsx
@@ -97,15 +97,15 @@ const QuincenaTest = (quincena) => {
 
 
        
-        // for (let index = +dateOne.substring(8,10); index <= +dateTwo.substring(8,10); index++) {
-        //     arrayD.push(index);
+        for (let index = +dateOne.substring(8,10); index <= +dateTwo.substring(8,10); index++) {
+            arrayD.push(index);
               
-        //   }
+          }
 
-        for (let index = +diaOne; index <= +diaTwo; index++) {
-          arrayD.push(index + "-" + mesOne + "-" + anioOne);
+        // for (let index = +diaOne; index <= +diaTwo; index++) {
+        //   arrayD.push(index + "-" + mesOne + "-" + anioOne);
             
-        }
+        // }
 
         for (let index = +diaOne; index <= +diaTwo; index++) {
             fechaD.push(index + "-" + mesOne + "-" + anioOne)
