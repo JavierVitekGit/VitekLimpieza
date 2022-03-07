547936c35dbc3d623baebe9c283636147e141eb6
Nhyix <raziel.tole@gmail.com>
2022-03-07 11:33:42 -0600

    Update QuincenaPrueba.jsx

--- src/QuincenaPrueba.jsx
+++ src/QuincenaPrueba.jsx
@@ -248,7 +248,7 @@ const QuincenaTest = (quincena) => {
 
                                                         
 
-                                                        if (item.Nombre == iter.Nombre && +iter.Fecha.substring(0,10) == +Object.keys(dialokobydiego)[0] ) {
+                                                        if (item.Nombre == iter.Nombre && +Object.values(iter.Fecha.substring(0,10)) == +Object.keys(dialokobydiego)[0] ) {
                                                             // console.log("Aqui se encontro algo :",{[Object.keys(dialokobydiego)[0]]:iter.Estado},"En el index:: ", index)
                                                             item.dias[index] = {[Object.keys(dialokobydiego)[0]]:iter.Estado+" "+iter.Justificacion+" "+iter.Suplencia}
                                                         }
