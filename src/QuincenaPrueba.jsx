86309535fc1054cf35eb24b4407bce3e3a1f33a0
Nhyix <raziel.tole@gmail.com>
2022-03-07 12:25:30 -0600

    Update QuincenaPrueba.jsx

--- src/QuincenaPrueba.jsx
+++ src/QuincenaPrueba.jsx
@@ -345,14 +345,14 @@ const QuincenaTest = (quincena) => {
     
     function getDays(){
         var days = [];
-        // for (let index = +dateOne.substring(8,10); index <= +dateTwo.substring(8,10); index++) {
-        //     days.push({[index.toString()]:"/"});
+        for (let index = +dateOne.substring(8,10); index <= +dateTwo.substring(8,10); index++) {
+            days.push({[index.toString()]:"/"});
 
-        // }
-
-        for (let i = diaOne; i <= diaTwo; i++ ){
-            days.push({[i.toString() + "-" + mesOne.toString() + "-" + anioOne.toString()]:"/"})
         }
+
+        // for (let i = diaOne; i <= diaTwo; i++ ){
+        //     days.push({[i.toString() + "-" + mesOne.toString() + "-" + anioOne.toString()]:"/"})
+        // }
 
         console.log("Days",days)
 
