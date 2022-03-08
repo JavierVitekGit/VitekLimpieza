1056c36c626210469547dab69b8a0e255e08d952
Nhyix <raziel.tole@gmail.com>
2022-03-08 16:55:30 -0600

    Update Calendario.jsx

--- src/Calendario.jsx
+++ src/Calendario.jsx
@@ -891,7 +891,7 @@ Ok
           options={unicos}
           sx={{width:"auto"}} 
           renderInput={(params) => <TextField {...params} label="Personal" />}
-          value={v=>v.target.value}
+          value={item.suplencia}
           // onChange={(_event,value)=>{setPorfa(value)}}
           onChange={v=>item.suplencia = v.target.value}
           id="AutocompletePersonal"
