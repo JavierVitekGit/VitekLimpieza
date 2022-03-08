e84afff914f4b0ccc14107759629adf1e151f766
Nhyix <raziel.tole@gmail.com>
2022-03-08 17:40:30 -0600

    Update Calendario.jsx

--- src/Calendario.jsx
+++ src/Calendario.jsx
@@ -891,9 +891,9 @@ Ok
           options={unicos}
           sx={{width:"auto"}} 
           renderInput={(params) => <TextField {...params} label="Personal" />}
-          value={v=>item.suplencia = v.target.value}
+          value={item.suplencia}
           // onChange={(_event,value)=>{setPorfa(value)}}
-          // onChange={v=>item.suplencia = v.target.value}
+          onChange={v=>item.suplencia = v.target.value}
           id="AutocompletePersonal"
           noOptionsText="Sin coincidencias"
           />
