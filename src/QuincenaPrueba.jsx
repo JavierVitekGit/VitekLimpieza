1a782b1fedee1eed25e4293fa0f4405dd3bc654d
Nhyix <raziel.tole@gmail.com>
2022-03-10 15:35:41 -0600

    Update QuincenaPrueba.jsx

--- src/QuincenaPrueba.jsx
+++ src/QuincenaPrueba.jsx
@@ -135,7 +135,7 @@ const QuincenaTest = (quincena) => {
     
         fechaD.forEach(fecha=>{
             console.log(`En ${fecha} fue ${diasSemana(fecha)}`)
-            diaSemanaArray.push(diasSemana(fecha))
+            diaSemanaArray.push((fecha.substring(8,10))+diasSemana(fecha))
         })
     
         
