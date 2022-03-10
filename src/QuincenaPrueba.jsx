e55c652ac7eb9af496f543e42619a6ba8c77a8f9
Nhyix <raziel.tole@gmail.com>
2022-03-10 11:33:09 -0600

    Update QuincenaPrueba.jsx

--- src/QuincenaPrueba.jsx
+++ src/QuincenaPrueba.jsx
@@ -168,7 +168,7 @@ const QuincenaTest = (quincena) => {
                         datos.push({Cliente:cl,Nombre:nm,Ingreso:fechaI,Baja:fechaB,Ubicacion:ubic,Horario:hr,Reasignacion:reasig,Puesto:puest, dias:getDays()})
                     // }
            
-                        
+                        console.log("fechaB",fechaB)
                    
              
                     datos.sort((a,b) => {
