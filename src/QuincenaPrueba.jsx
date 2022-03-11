0c1b427d5b7da04f54cee7f8e61513b2a63bff8e
Nhyix <raziel.tole@gmail.com>
2022-03-11 12:33:41 -0600

    Update QuincenaPrueba.jsx

--- src/QuincenaPrueba.jsx
+++ src/QuincenaPrueba.jsx
@@ -320,7 +320,7 @@ console.log("Datossd asda",datos)
                                                         
                                                         
 
-                                                        if (item.Nombre == iter.Nombre && +iter.Fecha.substring(0,2) == +Object.keys(dialokobydiego)[0] ) {
+                                                        if (item.Nombre == iter.Nombre && item.Nombre == iter.Cliente && +iter.Fecha.substring(0,2) == +Object.keys(dialokobydiego)[0] ) {
                                                             // console.log("Aqui se encontro algo :",{[Object.keys(dialokobydiego)[0]]:iter.Estado},"En el index:: ", index)
                                                             item.dias[index] = {[Object.keys(dialokobydiego)[0]]:iter.Estado+" "+iter.Justificacion+" "+iter.Suplencia}
                                                         }
