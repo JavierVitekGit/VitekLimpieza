048670d527e084d5106ba3819be3fff0262e3797
Nhyix <raziel.tole@gmail.com>
2022-03-14 11:28:31 -0600

    Update Reporte Factura.jsx

--- src/Reporte Factura.jsx
+++ src/Reporte Factura.jsx
@@ -337,7 +337,7 @@ console.log("Datossd asda",datos)
                                                         item.dias[index] = {[Object.keys(dialokobydiego)[0]]:item.Nombre}
                                                         
 
-                                                        if (item.Horario == iter.Turno && item.Nombre == iter.Nombre && item.Cliente == iter.Cliente && +iter.Fecha.substring(0,2) == +Object.keys(dialokobydiego)[0] ) {
+                                                        if (item.Horario == iter.Turno  && item.Cliente == iter.Cliente && +iter.Fecha.substring(0,2) == +Object.keys(dialokobydiego)[0] ) {
                                                             // console.log("Aqui se encontro algo :",{[Object.keys(dialokobydiego)[0]]:iter.Estado},"En el index:: ", index)
                                                             item.dias[index] = {[Object.keys(dialokobydiego)[0]]:iter.Nombre}
                                                         }
