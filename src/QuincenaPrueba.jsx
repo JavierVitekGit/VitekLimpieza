7b028bcc12a6f4759cb553c88aafa7ece1ebd046
Nhyix <raziel.tole@gmail.com>
2022-03-15 11:14:57 -0600

    Update QuincenaPrueba.jsx

--- src/QuincenaPrueba.jsx
+++ src/QuincenaPrueba.jsx
@@ -217,9 +217,9 @@ console.log("Datossd asda",datos)
                     // console.log("FechaBComp",fechaB.split("-"))0
 
 
-                        if (fechaB >= fBaja){
+                     
                             datos.push({Cliente:cl,Nombre:nm,Ingreso:fechaI,Baja:fechaB,Ubicacion:ubic,Horario:hr,Reasignacion:reasig,Puesto:puest, dias:getDays(),week:dias})
-                        }
+                            datos.filter((item)=> item.Baja >= fBaja)
 
                     
                         
