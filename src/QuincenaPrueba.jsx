9e4657c060001c3d25c550320a6a32a6cc0416c0
Nhyix <raziel.tole@gmail.com>
2022-03-15 15:08:54 -0600

    Update QuincenaPrueba.jsx

--- src/QuincenaPrueba.jsx
+++ src/QuincenaPrueba.jsx
@@ -217,10 +217,14 @@ console.log("Datossd asda",datos)
 
                     // console.log("FechaBComp",fechaB.split("-"))0
 
-
-                         if (Date(fechaB).getTime() <= Date(fBaja).getTime()){
+                        if (fechaB <= fBaja) {
                             datos.push({Cliente:cl,Nombre:nm,Ingreso:fechaI,Baja:fechaB,Ubicacion:ubic,Horario:hr,Reasignacion:reasig,Puesto:puest, dias:getDays(),week:dias})
-                         }
+                        }
+
+
+                        //  if (Date(fechaB) <= Date(fBaja[0]).getTime()){
+                        //     datos.push({Cliente:cl,Nombre:nm,Ingreso:fechaI,Baja:fechaB,Ubicacion:ubic,Horario:hr,Reasignacion:reasig,Puesto:puest, dias:getDays(),week:dias})
+                        //  }
 
                     
                         
