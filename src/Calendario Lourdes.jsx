4b152f3fd2801c73f2ddae7d0e9e8500f5c721ee
Nhyix <raziel.tole@gmail.com>
2022-03-22 08:31:15 -0600

    Update Calendario Lourdes.jsx

--- src/Calendario Lourdes.jsx
+++ src/Calendario Lourdes.jsx
@@ -302,26 +302,26 @@ const modClose = () => setModClient(false)
   function validate(event){
     event.preventDefault()
     
-    // arrayJusti.forEach((item)=>{
-    //   if (item.estado != "") {
-    //     if (item.justi ==  null){
-    //       handleShow(event);
-    //     } else {
-    //       show(event)
-    //       writeJustiData(event)
-    //     }
-
-    //   }
-    // })
-
-      arrayJusti.forEach((item)=>{
-        if (item.estado != "" && item.justi == null ){
+    arrayJusti.forEach((item)=>{
+      if (item.estado != "") {
+        if (item.justi ==  null){
           handleShow(event);
         } else {
           show(event)
           writeJustiData(event)
         }
-      })
+
+      }
+    })
+
+      // arrayJusti.forEach((item)=>{
+      //   if (item.estado != "" && item.justi == null ){
+      //     handleShow(event);
+      //   } else {
+      //     show(event)
+      //     writeJustiData(event)
+      //   }
+      // })
 
   }
 
