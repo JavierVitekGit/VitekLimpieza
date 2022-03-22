6f36396f11437682213278b4b571a4f2df7a993f
Nhyix <raziel.tole@gmail.com>
2022-03-22 12:12:56 -0600

    Update Quincena.jsx

--- src/Quincena.jsx
+++ src/Quincena.jsx
@@ -354,12 +354,9 @@ console.log("Datossd asda",datos)
                                                         // if (item.Nombre == iter.Nombre && +iter.Fecha.substring(0,2) == +Object.keys(dialokobydiego)[0] )
 
                                                         
-                                                        // console.log("nsubstring",n.substring(0,2))
-                                                        // console.log("NyxSubstring",nyx.substring(0,2))
+                                                        console.log("try$$%%:",dialokobydiego.slice(-2))
 
-                                                        // console.log("DaysSubstring",+Object.keys(dialokobydiego)[0].slice(-2))
-
-                                                        if (item.Horario == iter.Turno && item.Nombre == iter.Nombre && item.Cliente == iter.Cliente && +iter.Fecha.substring(0,2) == +Object.keys.slice(-2)(dialokobydiego)[0] ) {
+                                                        if (item.Horario == iter.Turno && item.Nombre == iter.Nombre && item.Cliente == iter.Cliente && +iter.Fecha.substring(0,2) == +Object.keys(dialokobydiego)[0] ) {
                                                             // console.log("Aqui se encontro algo :",{[Object.keys(dialokobydiego)[0]]:iter.Estado},"En el index:: ", index)
                                                             item.dias[index] = {[Object.keys(dialokobydiego)[0]]:iter.Estado+" "+iter.Justificacion}
                                                         }
